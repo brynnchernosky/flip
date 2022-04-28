c5f03bee85d1496b2598ed0802048c5627bdb94d
Adrian Chang <adrianchang@adrians-air.devices.brown.edu>
2022-04-28 12:24:49 -0400

    vis_pcd for folder

--- vis_pcd.py
+++ vis_pcd.py
@@ -1,16 +1,19 @@
 import numpy as np
 import open3d as o3d
 import argparse
+import os 
 
 def parseArguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filepath", type=str, default="test.csv", 
+    parser.add_argument("--filepath", type=str, default="", 
         help="filepath to csv")
+    parser.add_argument("--folder", type=str, default="", 
+        help="folder to csvs to iterate through")
     args = parser.parse_args()
     return args
 
-def main(args):
-    with open(args.filepath) as file:
+def vis_file(filepath):
+    with open(filepath) as file:
         lines = file.readlines()
     
     points = []
@@ -25,10 +28,19 @@ def main(args):
     pcd.points = o3d.utility.Vector3dVector(points)
     pcd.paint_uniform_color([0, 0, 1])
     print(points.shape[0])
-    print(pcd.get_center())
-    print(pcd.get_max_bound())
-    print(pcd.get_min_bound())
     o3d.visualization.draw_geometries([pcd])
+
+def main(args):
+    if args.filepath:
+        vis_file(args.filepath)
+    
+    if args.folder:
+        directory = os.fsencode(args.folder)
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            if filename.endswith(".csv"):
+                filepath = os.path.join(args.folder, filename)
+                vis_file(filepath)
 
 if __name__ == '__main__':
     args = parseArguments()
